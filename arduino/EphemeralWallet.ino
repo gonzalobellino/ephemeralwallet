59e292478e17f0161cd8fa22552222f930b9c8fd
nomorealias <nomorealias@gmail.com>
2020-04-28 02:04:20 -0300

    misspelling fixing

--- arduino/EphemeralWallet.ino
+++ arduino/EphemeralWallet.ino
@@ -23,15 +23,15 @@ const unsigned long IR_7_KEY = 0x00FF10EF;
 const unsigned long IR_AST_KEY = 0x00FF42BD;
 
 const int RECV_PIN = 11;
-const struct uECC_Curve_t * curve = uECC_secp256k1();  
+const struct uECC_Curve_t * curve = uECC_secp256k1();
 
 unsigned long optionSelected = IR_AST_KEY;
 String walletAddress = "N/A";
 String privateKey = "N/A";
-decode_results results;   
+decode_results results;
 IRrecv irrecv(RECV_PIN);
 
-//more addresses can be defined, for example  0x3F. Use I2C scanner if none of us work 
+//more addresses can be defined, for example  0x3F. Use I2C scanner if none of us work
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 //Overwritted RNG function for enhance random generation using Entropy library
@@ -47,7 +47,7 @@ static int RNG(uint8_t *dest, unsigned size) {
       --size;
     }
     return 1;
-  } 
+  }
 
 //Calculate free memory available
 int freeRAM() {
@@ -77,7 +77,7 @@ void showAtScreen(String msg, int line) {
 
   if (msg.length() > 16 && line == 0) {
     lcd.print(msg.substring(0,16));
-    lcd.setCursor(0,1);    
+    lcd.setCursor(0,1);
     lcd.print(msg.substring(16));
   } else {
       lcd.print(msg);
@@ -91,17 +91,17 @@ void lcdPrincipalMenu() {
   lcd.setCursor(0,0);
   lcd.print("1. Generate Priv");
   lcd.setCursor(0,1);
-  lcd.print("6. Show free mem");  
+  lcd.print("6. Show free mem");
 }
 
 void generateWallet() {
   uint8_t pk[32];
   uint8_t pub[64];
-  uint8_t *address = (uint8_t*)malloc(20);  
+  uint8_t *address = (uint8_t*)malloc(20);
 
-  randomSeed(Entropy.random());    
+  randomSeed(Entropy.random());
   uECC_make_key(pub, pk, curve);
-  
+
   uint8_t hashed[32];
   ethers_keccak256(pub, 64, hashed);
   memcpy(address, &hashed[12], 20);
@@ -110,17 +110,17 @@ void generateWallet() {
 
   //Serial.println(privateKey);
   //Serial.println(walletAddress);
-  //Serial.println("&" + privateKey + "!"); 
+  //Serial.println("&" + privateKey + "!");
   //Serial.println("#" + wallet + "$");
   //logSerial(wallet.c_str());
   //Serial.println("#" + wallet + "$");
-  //Serial.end() Serial.begin() controlar desde opciones del control remoto  
+  //Serial.end() Serial.begin() controlar desde opciones del control remoto
 }
 
 void setup() {
   //@todo change Serial to disabled by default
   Serial.begin(115200);
-  irrecv.enableIRIn(); 
+  irrecv.enableIRIn();
   lcd.init();
   lcd.backlight();
   uECC_set_rng(&RNG);
@@ -129,7 +129,7 @@ void setup() {
   // ensures that the standard libraries random number generator will provide different starting
   // values each time the sketch is run.  This performs much better than the normal
   // randomSeed(analogRead(0)).
-  randomSeed(Entropy.random());  
+  randomSeed(Entropy.random());
 
   lcdPrincipalMenu();
 }
@@ -148,39 +148,41 @@ void loop() {
       //Arrow Down> LCD Next page
       //Ok key> Confirmation
       //code list for Sony receiver --> https://naylampmechatronics.com/blog/36_Tutorial-Arduino-y-control-remoto-Infrarrojo.html
-      case IR_ARROW_UP_KEY: Serial.println("Arrow up"); 
+      case IR_ARROW_UP_KEY: Serial.println("Arrow up");
         if (optionSelected == IR_1_KEY or optionSelected == IR_2_KEY) {
           showAtScreen(privateKey.substring(0,32),0);
         }
         break;
-      case IR_OK_KEY: Serial.println("OK key");    
+      case IR_OK_KEY: Serial.println("OK key");
         break;
-      case IR_ARROW_DOWN_KEY: Serial.println("Arrow down"); 
+      case IR_ARROW_DOWN_KEY: Serial.println("Arrow down");
         if (optionSelected == IR_1_KEY or optionSelected == IR_2_KEY) {
           showAtScreen(privateKey.substring(32),0);
         }
         break;
-      case IR_1_KEY: Serial.println("Key 1: Generate pk");    
+      case IR_1_KEY: Serial.println("Key 1: Generate pk");
         optionSelected = IR_1_KEY;
         lcd.blink_on();
-        showAtScreen("Generating Private Key...",0);      
+        showAtScreen("Generating Private Key...",0);
         generateWallet();
         lcd.blink_off();
         showAtScreen(privateKey.substring(0,32),0);
          break;
       case IR_2_KEY: Serial.println("Key 2: Show Private key");
-        optionSelected = IR_2_KEY;  
-        showAtScreen(privateKey.substring(0,32),0);      
+        optionSelected = IR_2_KEY;
+        showAtScreen(privateKey.substring(0,32),0);
         break;
       case IR_3_KEY: Serial.println("Key 3: Show wallet address");
         optionSelected = IR_3_KEY;
-        showAtScreen(walletAddress,0);      
+        showAtScreen(walletAddress,0);
         break;
-      case IR_4_KEY: Serial.println("Key 4: Send PK throw default Serial port");    
+      case IR_4_KEY: Serial.println("Key 4: Send PK throw default Serial port");
         optionSelected = IR_4_KEY;
+        showAtScreen("Not implemented",0);        
         break;
       case IR_5_KEY: Serial.println("Key 5: Save PK in EEPROM memory");
         optionSelected = IR_5_KEY;
+        showAtScreen("Not implemented",0);
         break;
       case IR_6_KEY: Serial.println("Key 6: Show free memory");
         optionSelected = IR_6_KEY;
@@ -189,13 +191,13 @@ void loop() {
       case IR_7_KEY: Serial.println("Key 7: Clean memory");
         optionSelected = IR_7_KEY;
         showAtScreen("Not implemented",0);
-        break;                       
+        break;
       case IR_AST_KEY: Serial.println("Key *: Goto main menu");
         optionSelected = IR_AST_KEY;
         lcdPrincipalMenu();
-        break;                        
+        break;
     }
 
     irrecv.resume();
-  } 
+  }
 }
